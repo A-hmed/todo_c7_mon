722aa33a84386c028d909345df6add55489179a2
Ahmed <an0088039@gmail.com>
2022-12-12 19:10:04 +0200

    Finished firebase integration with android

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:todo_c7_mon/ui/screens/home/home.dart';
 import 'package:todo_c7_mon/utils/my_theme_data.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
